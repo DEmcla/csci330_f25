dda6eb08f3822909d7805002d4eb3a930f939a44
Owen Vareschi - CSCI330 <A30117192@mcla.edu>
2025-09-07 03:19:49 +0000

    Hello World

--- modules/module-01_ch1/assignment-template/src/main.cpp
+++ modules/module-01_ch1/assignment-template/src/main.cpp
@@ -1,16 +1,22 @@
+#include <ctime>
 #include <iostream>
 #include <string>
 
 int main() {
-    // TODO: Print "Hello, World!" to the console
-    
-    // TODO: Prompt user for their name
-    
-    // TODO: Read the user's name
-    
-    // TODO: Greet the user by name
-    
-    // BONUS TODO: Display current date
-    
-    return 0;
+  // TODO: Print "Hello, World!" to the console
+  std::cout << "Hello, world!" << std::endl;
+  // TODO: Prompt user for their name
+  std::cout << "What is your name?" << std::endl;
+  // TODO: Read the user's name
+  std::string name;
+  std::cin >> name;
+  // TODO: Greet the user by name
+  std::cout << "Hello, " + name << std::endl;
+  // BONUS TODO: Display current date
+  time_t timestamp;
+  time(&timestamp);
+
+  std::cout << ctime(&timestamp);
+
+  return 0;
 }
